3d042edfa5d7790aac9c32e3a7447d318bb56cdb
salva <salva.pucheibanez@gmail.com>
2022-12-22 16:29:28 +0100

    social media implemented in login and sign up

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,34 +1,32 @@
-import React, { useState, useContext, useEffect } from "react";
+import React, { useContext } from "react";
 import "./home.css";
 
 // Firebase
-import { auth, db } from "../database/base";
-import { signOut } from "firebase/auth";
+//import { auth, db } from "../database/base";
+// import { signOut } from "firebase/auth";
 import { AuthContext } from "../authentication/AuthProvider";
-import { ref, onValue } from "firebase/database";
+// import { ref, onValue } from "firebase/database";
 
 // Components
-import Login from "../components/auth/Login";
 import Dashboard from "../components/dashboard/Dashboard";
 import InitialButtons from "../components/auth/InitialButtons";
 
 function Home() {
   const { currentUser } = useContext(AuthContext);
-  const [username, setUsername] = useState("");
+  //const [username, setUsername] = useState("");
 
-  useEffect(() => {
-    console.log(currentUser);
-    if (currentUser) {
-      const starCountRef = ref(db, "users/" + currentUser.uid);
-      onValue(starCountRef, (snapshot) => {
-        if (snapshot.exists()) {
-          var data = snapshot.val();
-          console.log(data);
-          setUsername(data.firstName + " " + data.lastName);
-        }
-      });
-    }
-  }, [currentUser]);
+  // useEffect(() => {
+  //   if (currentUser) {
+  //     const starCountRef = ref(db, "users/" + currentUser.uid);
+  //     onValue(starCountRef, (snapshot) => {
+  //       if (snapshot.exists()) {
+  //         var data = snapshot.val();
+  //         console.log(data);
+  //         setUsername(data.firstName + " " + data.lastName);
+  //       }
+  //     });
+  //   }
+  // }, [currentUser]);
 
   return (
     <main className="home">
