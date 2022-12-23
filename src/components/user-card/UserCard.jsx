4bb5edd6f986511597265c5ba8a04cafbf645ad0
salva <salva.pucheibanez@gmail.com>
2022-12-23 12:27:54 +0100

    user settings panel created

--- src/components/user-card/UserCard.jsx
+++ src/components/user-card/UserCard.jsx
@@ -2,18 +2,31 @@ import React from "react";
 import "./userCard.css";
 
 // Bootstrap components
-import Card from "react-bootstrap/Card";
+import Button from "react-bootstrap/Button";
 
 function UserCard(props) {
   return (
-    <Card className="userCard">
-      <Card.Img variant="top" src={props.img} />
-      <Card.Body>
-        <Card.Title>{props.name}</Card.Title>
-        <Card.Text>{props.gender}</Card.Text>
-        <Card.Text>{props.specie}</Card.Text>
-      </Card.Body>
-    </Card>
+    <article className="userCard">
+      <img className="user--image" src={props.img} alt="avatar image" />
+      <section className="user--body">
+        <section className="user--body__column">
+          <h3 className="user--name">{props.name}</h3>
+          <p className="user--info">{props.gender}</p>
+          <p className="user--info">{props.specie}</p>
+        </section>
+        <section className="user--body__column">
+          <p className="user--info">Cuenta bancaria: </p>
+          <p className="user--info">Préstamo:</p>
+        </section>
+        <Button
+          variant="outline-primary"
+          className="user--actionButton"
+          onClick={props.openUserSettings}
+        >
+          Gestionar
+        </Button>
+      </section>
+    </article>
   );
 }
 
