3b57548804280fcd098254da75c4b570146a564b
salva <salva.pucheibanez@gmail.com>
2022-12-23 14:04:58 +0100

    users bank data added and formatted

--- src/components/user-card/UserCard.jsx
+++ src/components/user-card/UserCard.jsx
@@ -7,24 +7,28 @@ import Button from "react-bootstrap/Button";
 function UserCard(props) {
   return (
     <article className="userCard">
-      <img className="user--image" src={props.img} alt="avatar image" />
+      <img className="user--image" src={props.img} alt="" />
       <section className="user--body">
-        <section className="user--body__column">
-          <h3 className="user--name">{props.name}</h3>
-          <p className="user--info">{props.gender}</p>
-          <p className="user--info">{props.specie}</p>
+        <section className="user--body__header">
+          <h5 className="user--name">{props.name}</h5>
+          <Button
+            variant="outline-primary"
+            className="user--actionButton"
+            onClick={props.openUserSettings}
+          >
+            Gestionar
+          </Button>
         </section>
-        <section className="user--body__column">
-          <p className="user--info">Cuenta bancaria: </p>
-          <p className="user--info">Préstamo:</p>
+        <section className="user--body__main">
+          <section className="user--body__column">
+            <p className="user--info">{props.gender}</p>
+            <p className="user--info">{props.specie}</p>
+          </section>
+          <section className="user--body__column">
+            <p className="user--info">Cuenta bancaria: {props.bankAccount}</p>
+            <p className="user--info">Préstamo: {props.loan}</p>
+          </section>
         </section>
-        <Button
-          variant="outline-primary"
-          className="user--actionButton"
-          onClick={props.openUserSettings}
-        >
-          Gestionar
-        </Button>
       </section>
     </article>
   );
